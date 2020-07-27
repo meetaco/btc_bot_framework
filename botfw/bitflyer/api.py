6ec91f8da4e1f92c70ef2f57bc63bc42ed64e1d5
meetaco <iwamoto73@gmail.com>
2020-07-27 09:30:15 +0900

    BitflyerApi: implement RateLimitManager

--- botfw/bitflyer/api.py
+++ botfw/bitflyer/api.py
@@ -9,6 +9,7 @@ class BitflyerApi(ApiBase, ccxt.bitflyer):
     def __init__(self, ccxt_config={}):
         ApiBase.__init__(self)
         ccxt.bitflyer.__init__(self, ccxt_config)
+        self.rate_limit_manager = RateLimitManager()
         self.load_markets()
 
         # silence linter
@@ -47,3 +48,33 @@ class BitflyerApi(ApiBase, ccxt.bitflyer):
         market_id = self.market_id(symbol)
         return self.private_post_cancelallchildorders(
             {'product_code': market_id})
+
+    def request(self, path, api='public', method='GET', params={},
+                headers=None, body=None):
+        self.rate_limit_manager.add(path, api, params)
+        return super().request(path, api, method, params, headers, body)
+
+
+class RateLimitManager():
+    def __init__(self):
+        # import inside class due to:
+        # https://qiita.com/puriketu99/items/a1347bf5200f095e486e
+        from botutil.utils import TimeQueue
+        self.time_queue = TimeQueue(5 * 60)
+
+    def add(self, path, api, params):
+        call = {}
+        call['ip'] = True
+        call['private'] = api == 'private'
+        call['order'] = path in ['sendchildorder', 'sendparentorder', 'cancelallchildorders']
+        call['small'] = 'size' in params and params['size'] <= 0.1
+        self.time_queue.append(call)
+
+    def stat(self):
+        stat = {}
+        elements = self.time_queue.get_elements()
+        stat['ip'] = f"{len([x for x in elements if x['ip']])}/500 per 5m"
+        stat['private'] = f"{len([x for x in elements if x['private']])}/500 per 5m"
+        stat['order'] = f"{len([x for x in elements if x['order']])}/300 per 5m"
+        stat['small'] = f"{len([x for x in self.time_queue.get_elements(60) if x['small']])}/100 per 1m"
+        return str(stat)
